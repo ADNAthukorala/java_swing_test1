4116b2f3967c89cbf2d7367a9a98544fecf44c67
Dumindu Athukorala <117508037+ADNAthukorala@users.noreply.github.com>
2024-02-07 15:53:13 +0530

    Committed

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello ADNA!");
         JFrame frame = new JFrame();
 
         frame.setTitle("First Application");
