bbf7d95e48cafdb33a87ca32550ba796a525e720
Dumindu Athukorala <117508037+ADNAthukorala@users.noreply.github.com>
2024-02-08 11:12:53 +0530

    Committed

--- src/MyFrameJCheckBox.java
+++ src/MyFrameJCheckBox.java
@@ -1,18 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFrameJCheckBox extends JFrame {
+public class MyFrameJCheckBox extends JFrame implements ActionListener {
     JCheckBox isBold, isItalic;
     JTextField textField;
 
     MyFrameJCheckBox() {
-        textField = new JTextField(10);
-        isBold = new JCheckBox("Bold");
-        isItalic = new JCheckBox("Italic");
+        textField = new JTextField("ADNA",10);
+        isBold = new JCheckBox("Bold", false);
+        isItalic = new JCheckBox("Italic", false);
 
         this.setLayout(new FlowLayout());
         this.add(isBold);
         this.add(isItalic);
         this.add(textField);
+
+        isBold.addActionListener(this);
+        isItalic.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int bold = 0;
+        int italic = 0;
+
+        if(isBold.isSelected()) {
+            bold = Font.BOLD;
+        }
+        if (isItalic.isSelected()) {
+            italic = Font.ITALIC;
+        }
+
+        textField.setFont(new Font("Times New Roman", bold | italic, 12));
     }
 }
